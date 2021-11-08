34c30d57bfbe4dc47afaafd8c0dde07a3709e241
Leo Varghese <leovarghese@Leos-MBP-2.domain.name>
2021-11-08 19:01:08 +0530

    fix naming issues in main file

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import config
  
 def execute_exercises(module_name: str):
 
-    module = config.MODULES.get(test_file_name)
-    if module is None:
+    exercise_module = config.MODULES.get(module_name)
+    if exercise_module is None:
         raise NotImplementedError(f'Module {module_name} is not implemented yet.')
 
-    PATTERN = getattr(module, 'PATTERN')
-    TEST_CASES = getattr(module, 'TEST_CASES')
+    PATTERN = getattr(exercise_module, 'PATTERN')
+    TEST_CASES = getattr(exercise_module, 'TEST_CASES')
 
     utils.check_match_and_pretty_print(PATTERN, TEST_CASES)
 
 if __name__ == "__main__":
-    test_file_name = sys.argv[1] if len(sys.argv) >= 2 else 'e1'
-    execute_exercises(test_file_name)
+    exercise_module = sys.argv[1] if len(sys.argv) >= 2 else 'e1'
+    execute_exercises(exercise_module)
